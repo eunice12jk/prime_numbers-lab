8c85526daa26976c035ce58a61cc4d2bc71175e3
Eunice <euchiri@gmail.com>
2017-10-13 15:12:26 +0000

    Gives prime number

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,15 @@
-
+def primeNumbers(number):
+    prime_number_list = []
+    
+    for i in range(2, number+1):
+      prime = True
+    
+      for x in range(2,i):
+        if i % x == 0:
+          prime = False
+        
+      if prime == True:
+            
+        prime_number_list.append(i)
+        
+    return prime_number_list
